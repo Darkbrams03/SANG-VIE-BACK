2ff490b2b1f235f63e498b9733aff63b95b9f821
DARKBRAMS03 <erensmith2005@gmail.com>
2026-05-29 12:45:06 +0100

    Fix: Suppression du doublon CORS dans index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,5 @@
 <?php
 
-// 🔥 FORCE CORS POUR LA PRODUCTION (Vercel)
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With, X-Auth-Token");
-
-// Si c'est une requête de vérification OPTIONS, on valide immédiatement sans charger Laravel
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("HTTP/1.1 200 OK");
-    exit();
-}
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
